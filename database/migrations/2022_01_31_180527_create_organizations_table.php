ede4960b74784fa96d32b7cf2b7b30299efa169e
Jordi Gómez Hidalgo <gnugomez@gmail.com>
2022-01-31 19:26:36 +0100

    feat: added organization model and migrations

--- database/migrations/2022_01_31_180527_create_organizations_table.php
+++ database/migrations/2022_01_31_180527_create_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrganizationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
+            $table->string("name");
+            $table->string("description");
+            $table->string("address");
+            $table->string("phone");
+            $table->string("email");
+            $table->string("website");
+            $table->string("logo");
             $table->softDeletes();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Users');
+        Schema::dropIfExists('organizations');
     }
 }
---
+++ database/migrations/2022_01_31_181354_create_organization_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_user', function (Blueprint $table) {
+            $table->bigInteger('organization_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->unique(['organization_id', 'user_id']);
+            $table->foreign('organization_id')->references('id')->on('organizations')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_user');
+    }
+}
